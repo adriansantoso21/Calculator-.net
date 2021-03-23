6d2a91735108e3c9a716d057fa500457a83a1be1
adriansantoso21 <65168221+adriansantoso21@users.noreply.github.com>
2021-03-23 22:16:39 +0700

    Tugas 2 PBKK SImpleCalculator finish

---
+++ simpleCalculator/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace simpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tombolequal = new System.Windows.Forms.Button();
+            this.angka0 = new System.Windows.Forms.Button();
+            this.tombolkoma = new System.Windows.Forms.Button();
+            this.tombolC = new System.Windows.Forms.Button();
+            this.tombolminus = new System.Windows.Forms.Button();
+            this.angka1 = new System.Windows.Forms.Button();
+            this.angka2 = new System.Windows.Forms.Button();
+            this.angka3 = new System.Windows.Forms.Button();
+            this.tombolplus = new System.Windows.Forms.Button();
+            this.angka4 = new System.Windows.Forms.Button();
+            this.angka5 = new System.Windows.Forms.Button();
+            this.angka6 = new System.Windows.Forms.Button();
+            this.tombolkali = new System.Windows.Forms.Button();
+            this.angka7 = new System.Windows.Forms.Button();
+            this.angka8 = new System.Windows.Forms.Button();
+            this.angka9 = new System.Windows.Forms.Button();
+            this.tombolbagi = new System.Windows.Forms.Button();
+            this.hasiloutput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-176, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "testing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tombolequal
+            // 
+            this.tombolequal.Location = new System.Drawing.Point(230, 381);
+            this.tombolequal.Name = "tombolequal";
+            this.tombolequal.Size = new System.Drawing.Size(340, 57);
+            this.tombolequal.TabIndex = 1;
+            this.tombolequal.Text = "=";
+            this.tombolequal.UseVisualStyleBackColor = true;
+            this.tombolequal.Click += new System.EventHandler(this.tombolequal_Click);
+            // 
+            // angka0
+            // 
+            this.angka0.Location = new System.Drawing.Point(24, 308);
+            this.angka0.Name = "angka0";
+            this.angka0.Size = new System.Drawing.Size(136, 52);
+            this.angka0.TabIndex = 2;
+            this.angka0.Text = "0";
+            this.angka0.UseVisualStyleBackColor = true;
+            this.angka0.Click += new System.EventHandler(this.angka0_Click);
+            // 
+            // tombolkoma
+            // 
+            this.tombolkoma.Location = new System.Drawing.Point(230, 308);
+            this.tombolkoma.Name = "tombolkoma";
+            this.tombolkoma.Size = new System.Drawing.Size(136, 52);
+            this.tombolkoma.TabIndex = 3;
+            this.tombolkoma.Text = ",";
+            this.tombolkoma.UseVisualStyleBackColor = true;
+            this.tombolkoma.Click += new System.EventHandler(this.tombolkoma_Click);
+            // 
+            // tombolC
+            // 
+            this.tombolC.Location = new System.Drawing.Point(409, 308);
+            this.tombolC.Name = "tombolC";
+            this.tombolC.Size = new System.Drawing.Size(136, 52);
+            this.tombolC.TabIndex = 4;
+            this.tombolC.Text = "C";
+            this.tombolC.UseVisualStyleBackColor = true;
+            this.tombolC.Click += new System.EventHandler(this.tombolC_Click);
+            // 
+            // tombolminus
+            // 
+            this.tombolminus.Location = new System.Drawing.Point(598, 308);
+            this.tombolminus.Name = "tombolminus";
+            this.tombolminus.Size = new System.Drawing.Size(136, 52);
+            this.tombolminus.TabIndex = 5;
+            this.tombolminus.Text = "-";
+            this.tombolminus.UseVisualStyleBackColor = true;
+            this.tombolminus.Click += new System.EventHandler(this.tombolminus_Click);
+            // 
+            // angka1
+            // 
+            this.angka1.Location = new System.Drawing.Point(24, 250);
+            this.angka1.Name = "angka1";
+            this.angka1.Size = new System.Drawing.Size(136, 52);
+            this.angka1.TabIndex = 6;
+            this.angka1.Text = "1";
+            this.angka1.UseVisualStyleBackColor = true;
+            this.angka1.Click += new System.EventHandler(this.angka1_Click);
+            // 
+            // angka2
+            // 
+            this.angka2.Location = new System.Drawing.Point(230, 250);
+            this.angka2.Name = "angka2";
+            this.angka2.Size = new System.Drawing.Size(136, 52);
+            this.angka2.TabIndex = 7;
+            this.angka2.Text = "2";
+            this.angka2.UseVisualStyleBackColor = true;
+            this.angka2.Click += new System.EventHandler(this.angka2_Click);
+            // 
+            // angka3
+            // 
+            this.angka3.Location = new System.Drawing.Point(409, 250);
+            this.angka3.Name = "angka3";
+            this.angka3.Size = new System.Drawing.Size(136, 52);
+            this.angka3.TabIndex = 8;
+            this.angka3.Text = "3";
+            this.angka3.UseVisualStyleBackColor = true;
+            this.angka3.Click += new System.EventHandler(this.angka3_Click);
+            // 
+            // tombolplus
+            // 
+            this.tombolplus.Location = new System.Drawing.Point(598, 250);
+            this.tombolplus.Name = "tombolplus";
+            this.tombolplus.Size = new System.Drawing.Size(136, 52);
+            this.tombolplus.TabIndex = 9;
+            this.tombolplus.Text = "+";
+            this.tombolplus.UseVisualStyleBackColor = true;
+            this.tombolplus.Click += new System.EventHandler(this.tombolplus_Click);
+            // 
+            // angka4
+            // 
+            this.angka4.Location = new System.Drawing.Point(24, 192);
+            this.angka4.Name = "angka4";
+            this.angka4.Size = new System.Drawing.Size(136, 52);
+            this.angka4.TabIndex = 10;
+            this.angka4.Text = "4";
+            this.angka4.UseVisualStyleBackColor = true;
+            this.angka4.Click += new System.EventHandler(this.angka4_Click);
+            // 
+            // angka5
+            // 
+            this.angka5.Location = new System.Drawing.Point(230, 192);
+            this.angka5.Name = "angka5";
+            this.angka5.Size = new System.Drawing.Size(136, 52);
+            this.angka5.TabIndex = 11;
+            this.angka5.Text = "5";
+            this.angka5.UseVisualStyleBackColor = true;
+            this.angka5.Click += new System.EventHandler(this.angka5_Click);
+            // 
+            // angka6
+            // 
+            this.angka6.Location = new System.Drawing.Point(409, 192);
+            this.angka6.Name = "angka6";
+            this.angka6.Size = new System.Drawing.Size(136, 52);
+            this.angka6.TabIndex = 12;
+            this.angka6.Text = "6";
+            this.angka6.UseVisualStyleBackColor = true;
+            this.angka6.Click += new System.EventHandler(this.angka6_Click);
+            // 
+            // tombolkali
+            // 
+            this.tombolkali.Location = new System.Drawing.Point(598, 192);
+            this.tombolkali.Name = "tombolkali";
+            this.tombolkali.Size = new System.Drawing.Size(136, 52);
+            this.tombolkali.TabIndex = 13;
+            this.tombolkali.Text = "X";
+            this.tombolkali.UseVisualStyleBackColor = true;
+            this.tombolkali.Click += new System.EventHandler(this.tombolkali_Click);
+            // 
+            // angka7
+            // 
+            this.angka7.Location = new System.Drawing.Point(24, 134);
+            this.angka7.Name = "angka7";
+            this.angka7.Size = new System.Drawing.Size(136, 52);
+            this.angka7.TabIndex = 14;
+            this.angka7.Text = "7";
+            this.angka7.UseVisualStyleBackColor = true;
+            this.angka7.Click += new System.EventHandler(this.angka7_Click);
+            // 
+            // angka8
+            // 
+            this.angka8.Location = new System.Drawing.Point(230, 134);
+            this.angka8.Name = "angka8";
+            this.angka8.Size = new System.Drawing.Size(136, 52);
+            this.angka8.TabIndex = 15;
+            this.angka8.Text = "8";
+            this.angka8.UseVisualStyleBackColor = true;
+            this.angka8.Click += new System.EventHandler(this.angka8_Click);
+            // 
+            // angka9
+            // 
+            this.angka9.Location = new System.Drawing.Point(409, 134);
+            this.angka9.Name = "angka9";
+            this.angka9.Size = new System.Drawing.Size(136, 52);
+            this.angka9.TabIndex = 16;
+            this.angka9.Text = "9";
+            this.angka9.UseVisualStyleBackColor = true;
+            this.angka9.Click += new System.EventHandler(this.angka9_Click);
+            // 
+            // tombolbagi
+            // 
+            this.tombolbagi.Location = new System.Drawing.Point(598, 134);
+            this.tombolbagi.Name = "tombolbagi";
+            this.tombolbagi.Size = new System.Drawing.Size(136, 52);
+            this.tombolbagi.TabIndex = 17;
+            this.tombolbagi.Text = "/";
+            this.tombolbagi.UseVisualStyleBackColor = true;
+            this.tombolbagi.Click += new System.EventHandler(this.tombolbagi_Click);
+            // 
+            // hasiloutput
+            // 
+            this.hasiloutput.Location = new System.Drawing.Point(24, 12);
+            this.hasiloutput.Name = "hasiloutput";
+            this.hasiloutput.Size = new System.Drawing.Size(710, 98);
+            this.hasiloutput.TabIndex = 18;
+            this.hasiloutput.UseVisualStyleBackColor = true;
+            this.hasiloutput.Click += new System.EventHandler(this.hasiloutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hasiloutput);
+            this.Controls.Add(this.tombolbagi);
+            this.Controls.Add(this.angka9);
+            this.Controls.Add(this.angka8);
+            this.Controls.Add(this.angka7);
+            this.Controls.Add(this.tombolkali);
+            this.Controls.Add(this.angka6);
+            this.Controls.Add(this.angka5);
+            this.Controls.Add(this.angka4);
+            this.Controls.Add(this.tombolplus);
+            this.Controls.Add(this.angka3);
+            this.Controls.Add(this.angka2);
+            this.Controls.Add(this.angka1);
+            this.Controls.Add(this.tombolminus);
+            this.Controls.Add(this.tombolC);
+            this.Controls.Add(this.tombolkoma);
+            this.Controls.Add(this.angka0);
+            this.Controls.Add(this.tombolequal);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button tombolequal;
+        private System.Windows.Forms.Button angka0;
+        private System.Windows.Forms.Button tombolkoma;
+        private System.Windows.Forms.Button tombolC;
+        private System.Windows.Forms.Button tombolminus;
+        private System.Windows.Forms.Button angka1;
+        private System.Windows.Forms.Button angka2;
+        private System.Windows.Forms.Button angka3;
+        private System.Windows.Forms.Button tombolplus;
+        private System.Windows.Forms.Button angka4;
+        private System.Windows.Forms.Button angka5;
+        private System.Windows.Forms.Button angka6;
+        private System.Windows.Forms.Button tombolkali;
+        private System.Windows.Forms.Button angka7;
+        private System.Windows.Forms.Button angka8;
+        private System.Windows.Forms.Button angka9;
+        private System.Windows.Forms.Button tombolbagi;
+        private System.Windows.Forms.Button hasiloutput;
+    }
+}
+
---
+++ simpleCalculator/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+        string operand1 = string.Empty;
+        string operand2 = string.Empty;
+        char operation;
+        double result = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tombolequal_Click(object sender, EventArgs e)
+        {
+            operand2 = input;
+            double num1, num2;
+            double.TryParse(operand1, out num1);
+            double.TryParse(operand2, out num2);
+
+            if (operation == '+')
+            {
+                result = num1 + num2;
+                hasiloutput.Text = result.ToString();
+                input = result.ToString();
+            }
+            else if (operation == '-')
+            {
+                result = num1 - num2;
+                hasiloutput.Text = result.ToString();
+                input = result.ToString();
+            }
+            else if (operation == '*')
+            {
+                result = num1 * num2;
+                hasiloutput.Text = result.ToString();
+                input = result.ToString();
+            }
+            else if (operation == '/')
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    hasiloutput.Text = result.ToString();
+                    input = result.ToString();
+                }
+                else
+                {
+                    hasiloutput.Text = "DIV/Zero!";
+                }
+
+            }
+        }
+        private void tombolkoma_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += ".";
+            this.hasiloutput.Text += input;
+        }
+
+        private void tombolC_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+        }
+
+        private void tombolminus_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '-';
+            input = string.Empty;
+        }
+
+        private void tombolplus_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '+';
+            input = string.Empty;
+        }
+
+        private void tombolkali_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '*';
+            input = string.Empty;
+        }
+
+        private void tombolbagi_Click(object sender, EventArgs e)
+        {
+            operand1 = input;
+            operation = '/';
+            input = string.Empty;
+        }
+
+        private void hasiloutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void angka0_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "0";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka1_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "1";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka2_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "2";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka3_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "3";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka4_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "4";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka5_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "5";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka6_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "6";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka7_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "7";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka8_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "8";
+            this.hasiloutput.Text += input;
+        }
+
+        private void angka9_Click(object sender, EventArgs e)
+        {
+            this.hasiloutput.Text = "";
+            input += "9";
+            this.hasiloutput.Text += input;
+        }
+    }
+}
